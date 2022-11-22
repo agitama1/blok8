60f085409b361b1f8ede7491346dbcc0176d8bf8
agitama1 <93070268+agitama1@users.noreply.github.com>
2022-11-22 09:05:30 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "blok8.py",
+    version = "0.1",
+    py_modules = ['blok8'],
+    url = "https://github.com/agitama1/agitama1",
+    author = "demotomohiro",
+    install_requires = ["pyngrok"]
+)
